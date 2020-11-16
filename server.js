7500ad9f6bb788af7857fde4b29a7b46413c1d4a
Simon Otter <simonotter@gmail.com>
2020-11-16 16:14:15 +0000

    Added health endpoint

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const port = 8080;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
+
+// Health endpoint
+app.get('/heath', (req, res) => {
+  res.status(200).send("Healthy");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
